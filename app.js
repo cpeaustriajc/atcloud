3e31cc353c329e3596201f656525ad9b87b36346
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-17 21:32:01 +0800

    Remove jsdoc comments

--- app.js
+++ app.js
@@ -1,43 +1,3 @@
-/**
- * @typedef {Object} OpenWeatherResponse
- * @property {Object} coord
- * @property {number} coord.lon
- * @property {number} coord.lat
- * @property {Object[]} weather
- * @property {number} weather[].id
- * @property {string} weather[].main
- * @property {string} weather[].description
- * @property {string} weather[].icon
- * @property {string} base
- * @property {Object} main
- * @property {number} main.temp
- * @property {number} main.feels_like
- * @property {number} main.temp_min
- * @property {number} main.temp_max
- * @property {number} main.pressure
- * @property {number} main.humidity
- * @property {number} main.sea_level
- * @property {number} main.grnd_level
- * @property {number} visibility
- * @property {Object} wind
- * @property {number} wind.speed
- * @property {number} wind.deg
- * @property {number} wind.gust
- * @property {Object} clouds
- * @property {number} clouds.all
- * @property {number} dt
- * @property {Object} sys
- * @property {number} sys.type
- * @property {number} sys.id
- * @property {string} sys.country
- * @property {number} sys.sunrise
- * @property {number} sys.sunset
- * @property {number} timezone
- * @property {number} id
- * @property {string} name
- * @property {number} cod
- */
-
 const root = document.querySelector('#app')
 
 
@@ -46,13 +6,6 @@ loading.className = 'loading'
 loading.textContent = 'Loading...'
 root.appendChild(loading)
 
-/**
- * Fetches weather data from the API
- *
- * @param {number} lat Latitude
- * @param {number} lon Longitude
- * @returns {Promise<OpenWeatherResponse>} Weather data
- */
 async function getCurrentWeatherData(lat, lon) {
 	const res = await fetch('/api/weather', {
 		method: 'POST',
