2070900a3dffdf618daeaef56d8cc9eb37ecb819
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-17 21:29:33 +0800

    Use geolocation to get the region for the weather

--- api/weather.go
+++ api/weather.go
@@ -1,30 +1,97 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 )
 
+type WeatherData struct {
+	Weather []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+
+	Name string `json:"name"`
+
+	Cod int `json:"cod"`
+
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+
+	Sys struct {
+		Country string `json:"country"`
+	} `json:"sys"`
+
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+
+	Wind struct {
+		Speed float64 `json:"speed"`
+		Deg   float64 `json:"deg"`
+	} `json:"wind"`
+
+	Rain struct {
+		OneH   float64 `json:"1h"`
+		ThreeH float64 `json:"3h"`
+	} `json:"rain"`
+
+	Snow struct {
+		OneH   float64 `json:"1h"`
+		ThreeH float64 `json:"3h"`
+	} `json:"snow"`
+
+	Dt int `json:"dt"`
+
+	Id int `json:"id"`
+
+	Base string `json:"base"`
+
+	Visibility int `json:"visibility"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	api_key := os.Getenv("OPENWEATHERMAP_API_KEY")
 
-	response, err := http.Get("http://api.openweathermap.org/data/2.5/weather?lat=14.2161&lon=121.178&APPID=" + api_key)
-
-	if err != nil {
-		fmt.Fprintf(w, "Error: %s", err)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
+	var coords struct {
+		Lat float64 `json:"lat"`
+		Lon float64 `json:"lon"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&coords)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s", coords.Lat, coords.Lon, api_key)
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	defer response.Body.Close()
 
-	body, err := io.ReadAll(response.Body)
-
+	var weather WeatherData
+	err = json.NewDecoder(response.Body).Decode(&weather)
 	if err != nil {
-		fmt.Fprintf(w, "Error: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Fprintf(w, "%s", body)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(weather)
 }
--- app.js
+++ app.js
@@ -40,6 +40,12 @@
 
 const root = document.querySelector('#app')
 
+
+const loading = document.createElement('div')
+loading.className = 'loading'
+loading.textContent = 'Loading...'
+root.appendChild(loading)
+
 /**
  * Fetches weather data from the API
  *
@@ -48,22 +54,46 @@ const root = document.querySelector('#app')
  * @returns {Promise<OpenWeatherResponse>} Weather data
  */
 async function getCurrentWeatherData(lat, lon) {
-	const res = await fetch('/api/weather')
+	const res = await fetch('/api/weather', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ lat, lon })
+
+	})
 	const data = await res.json()
 
 	return data
 }
 
-class App {
-	render() {
-		const heading = document.createElement('h1')
-		heading.textContent = `Test`
-		heading.className = 'heading'
-
-		root.appendChild(heading)
-	}
+async function getCurrentLocation() {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	})
 }
 
-const app = new App()
+const { coords } = await getCurrentLocation()
 
-app.render()
+function toCelsius(temp) {
+	return temp - 273.15
+}
+
+const currentWeatherData = await getCurrentWeatherData(coords.latitude, coords.longitude)
+console.log(currentWeatherData)
+root.removeChild(loading)
+
+const weather = document.createElement('div')
+weather.className = 'weather'
+
+const city = document.createElement('h1')
+city.textContent = currentWeatherData.name
+city.className = 'heading'
+
+const temp = document.createElement('p')
+temp.textContent = toCelsius(currentWeatherData.main.temp).toFixed(2) + '°C'
+temp.className = 'temp'
+
+root.appendChild(weather)
+weather.appendChild(city)
+weather.appendChild(temp)
