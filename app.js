11d83b75b991057efb982418a2ac33f7b886ba9b
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-17 16:39:45 +0800

    Add jsdoc type for Open Weather Response

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+/**
+ * @typedef {Object} OpenWeatherResponse
+ * @property {Object} coord
+ * @property {number} coord.lon
+ * @property {number} coord.lat
+ * @property {Object[]} weather
+ * @property {number} weather[].id
+ * @property {string} weather[].main
+ * @property {string} weather[].description
+ * @property {string} weather[].icon
+ * @property {string} base
+ * @property {Object} main
+ * @property {number} main.temp
+ * @property {number} main.feels_like
+ * @property {number} main.temp_min
+ * @property {number} main.temp_max
+ * @property {number} main.pressure
+ * @property {number} main.humidity
+ * @property {number} main.sea_level
+ * @property {number} main.grnd_level
+ * @property {number} visibility
+ * @property {Object} wind
+ * @property {number} wind.speed
+ * @property {number} wind.deg
+ * @property {number} wind.gust
+ * @property {Object} clouds
+ * @property {number} clouds.all
+ * @property {number} dt
+ * @property {Object} sys
+ * @property {number} sys.type
+ * @property {number} sys.id
+ * @property {string} sys.country
+ * @property {number} sys.sunrise
+ * @property {number} sys.sunset
+ * @property {number} timezone
+ * @property {number} id
+ * @property {string} name
+ * @property {number} cod
+ */
+
 class App {
 	constructor() {
 		this.root = document.querySelector('#app')
