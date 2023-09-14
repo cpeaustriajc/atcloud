39e7adfdedc221ce609b2988994de887cea25003
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-09-14 15:19:17 +0800

    Remove edge runtime and show data

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import { CurrentWeatherResponse } from "./weather.types";
 
-export const runtime = "edge"
-
 export default async function Home() {
   const res = await fetch("/api/weather");
 
@@ -10,12 +8,12 @@ export default async function Home() {
     throw new Error("Failed to get the latest weather data.");
   }
 
-  const data = await res.json() as CurrentWeatherResponse;
+  const data = (await res.json()) as CurrentWeatherResponse;
 
   return (
     <div>
-      <h1>{data.base}</h1>
-      <p>Temperature</p>
+      <h1>{data.name}</h1>
+      <p>{data.main.temp}</p>
     </div>
   );
 }
