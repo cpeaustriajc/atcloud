65a4e45e371efe8e5956560a022f5460e81bd9fc
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-04 22:35:30 +0800

    Update clock component

--- apps/web/app/clock.tsx
+++ apps/web/app/clock.tsx
@@ -3,24 +3,25 @@
 import { useEffect, useState } from "react";
 
 export default function Clock() {
-  const [time, setTime] = useState<string>(() => "");
+	const [time, setTime] = useState<string>(() => "");
 
-  useEffect(() => {
-    const now = new Date().toLocaleTimeString("en-PH", {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    });
-    const interval = setInterval(() => {
-      setTime(now);
-    }, 1000);
+	// biome-ignore lint: time is needed to be in dependency array to update every tick
+	useEffect(() => {
+		const now = new Date().toLocaleTimeString("en-PH", {
+			hour: "2-digit",
+			minute: "2-digit",
+			second: "2-digit",
+		});
+		const interval = setInterval(() => {
+			setTime(now);
+		}, 1000);
 
-    return () => clearInterval(interval);
-  }, [time]);
+		return () => clearInterval(interval);
+	}, [time]);
 
-  return (
-    <div className="h-16">
-      <time className="text-6xl font-mono">{time}</time>
-    </div>
-  );
+	return (
+		<div className="h-16">
+			<time className="text-6xl font-mono">{time}</time>
+		</div>
+	);
 }
